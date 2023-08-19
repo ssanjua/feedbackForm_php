4e72e28d4dde3313d5ce7b7cd900f92d26251798
paupallares <ppaupallares@gmail.com>
2023-08-19 13:19:48 +1000

    uploaded port mysql

--- config/database.php
+++ config/database.php
@@ -4,9 +4,10 @@
     define('DB_USER', 'root');
     define('DB_PASS', 'tbCKNGlBvhk6CLXSXeoS');
     define('DB_NAME', 'railway');
+    define('DB_PORT', 7018); 
 
     //create connection
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
     //check connection
     if($conn->connect_error) {
